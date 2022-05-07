39d0063988c618c9ea20913b6b5842c8588fc0b9
abhisheksatpute369 <abhisheksatpute99@gmail.com>
2022-05-07 13:53:59 +0530

    create navbar and searchbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import Country from "./components/country";
+
 import './App.css';
 
 function App() {
   return (
-    
-    <div></div>
+   <div>
+      <Country />
+    </div>
   );
 }
 
---
+++ src/components/country.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./country.css"
+var Country = ()=>{
+    return(
+        <div> 
+            <div id="container">
+                <div id = "searchbar">
+                        <input type = "text" name="name" id="search-input" placeholder="Enter Country"></input>
+                        <button id="search-btn">Search</button>
+                </div>
+                <div id = "countrydisplay">
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Country;
