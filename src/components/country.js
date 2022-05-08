ce7cf250933de1f7410471412dca5144a5f0797c
abhisheksatpute369 <abhisheksatpute99@gmail.com>
2022-05-08 11:51:16 +0530

    apply filter functionality

--- src/components/country.js
+++ src/components/country.js
@@ -30,7 +30,7 @@ var Country = ()=>{
 
     // for sorting the data 
     const handlesortmethod = () =>{
-        var result = document.getElementById("selectsort").value;
+        const result = document.getElementById("selectsort").value;
         if(result === "low"){
             setcountry([]);
             setcountry(country.sort((a,b)=> a.population-b.population ))
@@ -45,7 +45,34 @@ var Country = ()=>{
 
     // these is for handle filter method 
     const handlefiltermethod = ()=>{
+        const res = document.getElementById("selectfilter").value;
+        if(res === "AtoZ"){
+            setcountry([]);
+            setcountry(country.sort((a,b)=> {
+                const na = a.name.toUpperCase();
+                const nb = b.name.toUpperCase();
+                if(na<nb) {return -1}
+                if(na>nb) {return 1}
+                return 0;
+            } ))
+            
+        }
+        else if(res === "ZtoA"){
+            setcountry([]);
+            setcountry(country.sort((a,b)=> {
+                const na = a.name.toUpperCase();
+                const nb = b.name.toUpperCase();
+                if(na<nb) {return 1}
+                if(na>nb) {return -1}
+                return 0;
+            } ))
 
+        }
+    }
+
+    // thse is for handle the input country 
+    const handlesubmit = (e) =>{
+        e.preventDefault();
     }
 
     if(loading)
@@ -57,16 +84,20 @@ var Country = ()=>{
             <div id="container">
                 {/* these div for upper navbar */}
                 <div id = "searchbar">
+                    <form onSubmit={handlesubmit} id ="searchform">
+                    {/* <div id="searchdiv"> */}
                     <input type = "text" name="name" id="search-input" placeholder="Enter Country"></input>
-                    <button id="search-btn">Search</button> 
+                    <button id="search-btn" type="submit">Search</button> 
+                    {/* </div> */}
+                    </form>
                     {/* these is for sorting method by population */}
-                    <select id = "selectsort" onClick={handlesortmethod}>
+                    <select id = "selectsort" onChange={handlesortmethod}>
                         <option >Sort Population</option>
                         <option value = {"low"}>Low to High</option>
                         <option value={"high"}>High to Low</option>
                     </select>
                     {/* these is for filter method for name ascending and descending  */}
-                    <select id = "selectfilter" onClick={handlefiltermethod}>
+                    <select id = "selectfilter" onChange={handlefiltermethod}>
                         <option >Filter</option>
                         <option value = {"AtoZ"}>A to Z</option>
                         <option value={"ZtoA"}>Z to A</option>
