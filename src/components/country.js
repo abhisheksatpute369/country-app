94c6442889009b68f25d5ff823dec437ab71c07c
abhisheksatpute369 <abhisheksatpute99@gmail.com>
2022-05-08 10:30:53 +0530

    some chnage in sort and add filter

--- src/components/country.js
+++ src/components/country.js
@@ -26,17 +26,25 @@ var Country = ()=>{
     const indexoffirstpost = indexoflastpage - postperpage;
     const currentpost = country.slice(indexoffirstpost, indexoflastpage);
 
-    const paginate = (pagenumber)=> setcurrentpage(pagenumber)
+    var paginate = (pagenumber)=> setcurrentpage(pagenumber)
 
+    // for sorting the data 
     const handlesortmethod = () =>{
         var result = document.getElementById("selectsort").value;
         if(result === "low"){
+            setcountry([]);
             setcountry(country.sort((a,b)=> a.population-b.population ))
-             
+            
         }
         else if(result === "high"){
+            setcountry([]);
             setcountry(country.sort((a,b)=> b.population - a.population))
+
         }
+    }
+
+    // these is for handle filter method 
+    const handlefiltermethod = ()=>{
 
     }
 
@@ -49,13 +57,20 @@ var Country = ()=>{
             <div id="container">
                 {/* these div for upper navbar */}
                 <div id = "searchbar">
-                        <input type = "text" name="name" id="search-input" placeholder="Enter Country"></input>
-                        <button id="search-btn">Search</button>                
-                        <select id = "selectsort" onClick={handlesortmethod}>
-                            <option >Sort Population</option>
-                            <option value = {"low"}>Low to High</option>
-                            <option value={"high"}>High to Low</option>
-                        </select>
+                    <input type = "text" name="name" id="search-input" placeholder="Enter Country"></input>
+                    <button id="search-btn">Search</button> 
+                    {/* these is for sorting method by population */}
+                    <select id = "selectsort" onClick={handlesortmethod}>
+                        <option >Sort Population</option>
+                        <option value = {"low"}>Low to High</option>
+                        <option value={"high"}>High to Low</option>
+                    </select>
+                    {/* these is for filter method for name ascending and descending  */}
+                    <select id = "selectfilter" onClick={handlefiltermethod}>
+                        <option >Filter</option>
+                        <option value = {"AtoZ"}>A to Z</option>
+                        <option value={"ZtoA"}>Z to A</option>
+                    </select>
                 </div>
                 {/* these div is for country display  */}
                 <div id = "countrydisplay">
